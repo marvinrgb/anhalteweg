902aab7a4c72628c9e7d0655aba831edc81feb1e
Marvin Wolfart <wolfart.m@loh-services.de>
2023-04-13 15:55:47 +0200

    additions

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# anhalteweg
+# Anhalteweg
+Veranschaulichung des Anhaltewegs
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const pxPerM = 10
+document.getElementById('fussball').style.width = 105 * pxPerM + 'px'
 
 let num = 50;
 
 function getBrakingWeg(v) {
-  return (v / 10) * (v / 10);
+  return (((v / 10) * (v / 10)) / 2);
 }
 function getReactionLength(v) {
   return (v / 10) * 3;
@@ -12,15 +13,35 @@ function getReactionLength(v) {
 function calculate(v) {
   let reaction = document.getElementById('reaction');
   let braking = document.getElementById('braking');
+  let anhalteweg = getBrakingWeg(v) + getReactionLength(v);
 
   reaction.style.width = getReactionLength(v) * pxPerM + 'px';
-  reaction.innerHTML = getReactionLength(v) * pxPerM + 'm';
-  
+  reaction.innerHTML = (Math.round(getReactionLength(v) * 10) / 10) + 'm';
+  console.log(getBrakingWeg(v))
   braking.style.width = getBrakingWeg(v) * pxPerM + 'px';
-  braking.innerHTML = Math.round(getBrakingWeg(v) * pxPerM) + 'm';
+  braking.innerHTML = (Math.round(getBrakingWeg(v) * 10) / 10) + 'm';
+
+  let carcon = document.getElementById('car-con');
+  carcon.innerHTML = '';
+  let car = document.createElement('img');
+  car.src = 'https://static.vecteezy.com/system/resources/thumbnails/016/475/159/small_2x/red-suv-free-vector.jpg';
+  car.classList.add('car')
+  car.style.width = 4.5 * pxPerM + 'px'
+
+  for (let i = 0; i < anhalteweg; i += 4.5) {
+    carcon.appendChild(car.cloneNode(true))
+  }
 
 }
 
+
+let carcon = document.getElementById('car-con');
+carcon.innerHTML = '';
+let car = document.createElement('img');
+car.src = 'https://static.vecteezy.com/system/resources/thumbnails/016/475/159/small_2x/red-suv-free-vector.jpg';
+car.classList.add('car')
+car.style.width = 4.5 * pxPerM + 'px'
+carcon.appendChild(car)
 
 document.getElementById('input-v').addEventListener('change', (event) => {
   calculate(event.target.value);
